93cf7a0d3766d3715f6a4dbd9e7a221a13796a4d
a0trox <145950871+a0trox@users.noreply.github.com>
2025-11-12 18:01:04 +0800

    version1
    It's a web demo

---
+++ duskin_project1/src/main/java/com/example/duskin_project1/DuskinProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.duskin_project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuskinProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuskinProject1Application.class, args);
+	}
+
+}
---
+++ duskin_project1/src/test/java/com/example/duskin_project1/DuskinProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.duskin_project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuskinProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
